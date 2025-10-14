d780e0aa5122c8d0a4bdb6e502b33032d801e341
gg <georgevanderveen@yahoo.fr>
2025-10-14 15:12:49 +0200

    Implement AddAccount and DeleteAccount  methods to Bank

--- vanderveengeorge/Bank.cs
+++ vanderveengeorge/Bank.cs
@@ -1,13 +1,27 @@
 class Bank(Dictionary<string, CurrentAccount> accounts, string name)
 {
-    public Dictionary<string, CurrentAccount> Accounts { get; } = accounts;
+    public Dictionary<string, CurrentAccount> Accounts { get; private set; } = accounts;
     public string Name { get; set; } = name;
-    void AddAccount(CurrentAccount account)
+    public void AddAccount(CurrentAccount account)
     {
-
+        if (Accounts.ContainsKey(account.Number))
+        {
+            Console.WriteLine("Le compte existe déjà !");
+        }
+        else
+        {
+            Accounts.Add(account.Number, account);
+        }
     }
-    void DeleteAccount(string number)
+    public void DeleteAccount(string number)
     {
-
+        if (Accounts.ContainsKey(number))
+        {
+            Accounts.Remove(number);
+        }
+        else
+        {
+            Console.WriteLine("Le compte n'existe pas !");
+        }
     }
 }
